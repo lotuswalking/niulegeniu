0b49832c06ac50f3aa1136527050530bd4b9f262
junyan li <jiangzhonping@163.com>
2022-09-23 22:32:23 -0400

    add dan as new image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import paul from "./images/paul.JPG";
+import dan from "./images/dan.JPG";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Card, Image } from "react-bootstrap";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Card style={{ width: "80px" }}>
       <Card.Img variant="bottom rounded" src={paul} />
+      <Card.Img variant="bottom rounded" src={dan} />
     </Card>
   );
 }
