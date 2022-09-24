5207eac0b8b5902c034af27f22c139379778b7ee
junyan li <jiangzhonping@163.com>
2022-09-23 22:02:13 -0400

    add button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import paul from "./images/paul.JPG";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+      <Button type="button" class="btn btn-primary">
+        Button
+      </Button>
       This is an App
+      <br />
       <img src={paul} alt="NoImg" />
     </div>
   );
