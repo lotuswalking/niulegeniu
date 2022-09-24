5c7edde9b6df45b67261c6553661dac2a6157624
junyan li <jiangzhonping@163.com>
2022-09-23 21:31:45 -0400

    update audio and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import paul from "./images/paul.JPG";
 
 function App() {
-  return <div className="App">This is an App</div>;
+  return (
+    <div className="App">
+      This is an App
+      <img src={paul} />
+    </div>
+  );
 }
 
 export default App;
